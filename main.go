b9d7a69a93df62b8dc3394531b08fbb7b6421787
DerrickKirimi <remmydren@gmail.com>
2023-03-15 12:56:10 +0300

    Basic

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
-package main
+package main 
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
-func main() {
-	fmt.Println("Hello World")
+func home (w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippets"))
 }
 
-//run with go run . //
+func main () {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err) 
+}
