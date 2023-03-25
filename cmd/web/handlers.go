4e0c9880453f7893478a0f892b5d907095088f21
DerrickKirimi <remmydren@gmail.com>
2023-03-25 07:08:04 +0300

    Partials

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -17,6 +17,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	//initialise a slice containing the paths to our two files with the base template as the first
 	files := []string{
 		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
 		"./ui/html/pages/home.tmpl",
 	}
 
