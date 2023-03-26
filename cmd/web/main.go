a8d66cfac5f64cbd2dff739a640866f0df651a3a
DerrickKirimi <remmydren@gmail.com>
2023-03-26 20:06:35 +0300

    Logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,19 +1,39 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "Http network address")
+
+	//parse into addr variable. Terminate application on error.
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
+
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	//same network addr and routes as before + ErrorLog field set
+	srv := &http.Server{
+		Addr:		*addr,
+		ErrorLog:	errorLog,
+		Handler:	mux,
+
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
