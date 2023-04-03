121c338ca32904528fbd2f8a22ebc93130ed0603
DerrickKirimi <remmydren@gmail.com>
2023-04-03 16:05:39 +0300

    Refactor

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -9,9 +9,9 @@ func (app *application) routes() *http.ServeMux{
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Handle("/static", http.StripPrefix("/static", fileServer))
 
-	mux.Handle("/", app.home)
-	mux.Handle("/snippet/view", app.snippetView)
-	mux.Handle("/snipet/create", app.snippetCreate)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snipet/create", app.snippetCreate)
 
 	return mux
 }
