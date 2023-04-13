7e2d77fa88e43fdf136ba02f64a6d2f1bfa25d3d
DerrickKirimi <remmydren@gmail.com>
2023-04-13 13:13:20 +0300

    Justinas/alice

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,17 +1,18 @@
 package main 
 import (
 	"net/http"
+	"github.com/justinas/alice"
 )
 
-func (app *application) routes() http.Handler {
+func (app *application) routes () http.Handler {
 	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-	mux.Handle("/static", http.StripPrefix("/static", fileServer))
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snipet/create", app.snippetCreate)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+	return standard.Then(mux)
 }
