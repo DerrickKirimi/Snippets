62a997c2bcec902b2680b796dd4bdf63b70d5439
DerrickKirimi <remmydren@gmail.com>
2023-04-19 16:02:05 +0300

    Validate User Input

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -80,6 +80,8 @@ type snippetCreateForm struct {
 	validator.Validator		`form:"-"`
 }
 
+
+
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	
 	var form snippetCreateForm
@@ -113,12 +115,44 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
 
+type UserSignupForm struct {
+	Name 				string `form:"name"`
+	Email				string `form:"email"`
+	Password			string `form:"password"`
+	validator.Validator `form:"-"`
+
+}
+
 func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(w, "Display a html form for signing up a new user..")
+	data := app.newTemplateData(r)
+	data.Form = UserSignupForm{}
+	app.render(w, http.StatusOK, "signup.tmpl", data)
 }
 
 func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Create a new user...")
+	var form UserSignupForm
+
+	err := app.decodePostForm(r, &form)
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	form.CheckField(validator.NotBlank(form.Name), "name", "This field cannot be blank")
+	form.CheckField(validator.NotBlank(form.Email), "email", "This field cannot be blank")
+	form.CheckField(validator.Matches(form.Email, validator.EmailRX), "email", "This field must be a valid email address")
+	form.CheckField(validator.NotBlank(form.Password), "password", "This field cannot be blank")
+	form.CheckField(validator.MinChars(form.Password, 8), "password", "This field must be at least 8 characters long")
+	
+	if !form.Valid(){
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, http.StatusUnprocessableEntity, "signup.tmpl", data)
+		return
+	}
+
+	fmt.Println(w, "create a new user ...")
+
 }
 func (app *application) userLogin(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Display a HTML form for logging in a user...")
